6e7826c05507b27ed777462d8ba0f828f193c7ad
Johnny <johnny_s.o@hotmail.com>
2022-11-27 19:08:20 -0300

    Todo List adicionando novas opções conforme o que é inserido no input

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Todo(){
+  const [task, setTask] = useState("");
+  const [itemsList, setItemsList] = useState([]);
+
   return (
     <div className="todo-wrapper">
       <h1>ToDo List</h1>
-      <form>
-        <input type="text" placeholder="Adicione uma tarefa"/>
+      <form onSubmit={handleAddItemToList}>
+        <input type="text" placeholder="Adicione uma tarefa" onChange={handleChangeInput}/>
         <button type="submit">Adicionar</button>
       </form>
 
       <ul className="todo-list">
-        <li>Item</li>
+        {itemsList.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
       </ul>
     </div>
   );
+
+  function handleChangeInput(event) {
+    const inputTask = event.target.value;
+
+    setTask(inputTask);
+ }
+
+ function handleAddItemToList(event){
+  event.preventDefault();
+
+  setItemsList([...itemsList, task])
+ }
+
 }
+
+export default Todo;
