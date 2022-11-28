d1593c09a743098f9701df84d67d91ac09190131
Johnny <johnny_s.o@hotmail.com>
2022-11-28 07:45:38 -0300

    Estilização aplicada em form, todo e app.

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './form.css'
 
 function Form(props){
   return (
-    <form onSubmit={props.onSubmit}>
+    <form onSubmit={props.onSubmit} className="body_form">
       {props.children}
     </form>
   )
