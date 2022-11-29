0a45a701ee9fd41b49437b30461a4acef7e7648d
Johnny <johnny_s.o@hotmail.com>
2022-11-29 08:26:46 -0300

    Adicionando bibilioteca de icon da React Icons e colocado lixeira na lista

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './list.css'
+import { AiFillDelete } from 'react-icons/ai';
+
 
 function List(props) {
   return (
     <ul className="todo-list">  
       {props.itemsList.map((item, index) => (
-        <li key={index}>{item}</li>
+        <li key={index}>{item} <AiFillDelete /></li>
       ))}
     </ul>
   )
