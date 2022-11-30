e0c7429c5a27b0e527aa61e38f590ad3af4ac887
Johnny <johnny_s.o@hotmail.com>
2022-11-30 06:43:30 -0300

    Ajustando icones de Verificado e de delete

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './list.css'
 import { AiFillDelete } from 'react-icons/ai';
+import { MdVerified } from 'react-icons/md';
+
 
 
 function List(props) {
   return (
     <ul className="todo-list">  
       {props.itemsList.map((item, index) => (
-        <li key={index}>{item} <AiFillDelete /></li>
+        <li key={index}>{item} 
+          <div className="icons">
+            <MdVerified id="icon_verified"/>
+            <AiFillDelete id="icon_delete"/>
+          </div>
+        </li>
       ))}
     </ul>
   )
